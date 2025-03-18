95433cbcd68bbdc7b259d0eaab5bef1ebe0c585b
_ <ryancpersonal10@gmail.com>
2025-03-18 17:42:50 -0400

    Please work

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  basePath: '/landing-page',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
